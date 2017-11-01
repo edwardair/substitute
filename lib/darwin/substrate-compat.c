0245c1965675561d97b2d5472d52b083163b4500
Eduoduo_RayMi <550621009@qq.com>
2017-11-01 15:02:54 +0800

    0、主要解决iOS11上dyld_get_all_infos函数找不到，导致插件加载失败问题 1、修复pc类型导致无法编译问题 2、修复substrate.h ARC 环境指针错误 3、更改Sub**** 为 MS****，以兼容旧的libsubstrate.dylib

--- lib/darwin/substrate-compat.c
+++ lib/darwin/substrate-compat.c
@@ -4,16 +4,16 @@
 #include <mach-o/dyld.h>
 
 EXPORT
-void *SubGetImageByName(const char *filename) __asm__("SubGetImageByName");
-void *SubGetImageByName(const char *filename) {
+void *MSGetImageByName(const char *filename) __asm__("_MSGetImageByName");
+void *MSGetImageByName(const char *filename) {
     return substitute_open_image(filename);
 }
 
 EXPORT
-void *SubFindSymbol(void *image, const char *name) __asm__("SubFindSymbol");
-void *SubFindSymbol(void *image, const char *name) {
+void *MSFindSymbol(void *image, const char *name) __asm__("_MSFindSymbol");
+void *MSFindSymbol(void *image, const char *name) {
     if (!image) {
-        const char *s = "SubFindSymbol: 'any image' specified, which is incredibly slow - like, 2ms on a fast x86.  I'm going to do it since it seems to be somewhat common, but you should be ashamed of yourself.";
+        const char *s = "MSFindSymbol: 'any image' specified, which is incredibly slow - like, 2ms on a fast x86.  I'm going to do it since it seems to be somewhat common, but you should be ashamed of yourself.";
         syslog(LOG_WARNING, "%s", s);
         fprintf(stderr, "%s\n", s);
         /* and it isn't thread safe, but neither is MS */
@@ -24,7 +24,7 @@ void *SubFindSymbol(void *image, const char *name) {
                 fprintf(stderr, "(btw, couldn't open %s?)\n", im_name);
                 continue;
             }
-            void *r = SubFindSymbol(im, name);
+            void *r = MSFindSymbol(im, name);
             substitute_close_image(im);
             if (r)
                 return r;
@@ -40,27 +40,27 @@ void *SubFindSymbol(void *image, const char *name) {
 
 #ifdef TARGET_DIS_SUPPORTED
 EXPORT
-void SubHookFunction(void *symbol, void *replace, void **result)
-    __asm__("SubHookFunction");
-void SubHookFunction(void *symbol, void *replace, void **result) {
+void MSHookFunction(void *symbol, void *replace, void **result)
+    __asm__("_MSHookFunction");
+void MSHookFunction(void *symbol, void *replace, void **result) {
     struct substitute_function_hook hook = {symbol, replace, result};
     int ret = substitute_hook_functions(&hook, 1, NULL,
                                         SUBSTITUTE_NO_THREAD_SAFETY);
     if (ret) {
-        substitute_panic("SubHookFunction: substitute_hook_functions returned %s\n",
+        substitute_panic("MSHookFunction: substitute_hook_functions returned %s\n",
                          substitute_strerror(ret));
     }
 }
 #endif
 
 EXPORT
-void SubHookMessageEx(Class _class, SEL sel, IMP imp, IMP *result)
-    __asm__("SubHookMessageEx");
+void MSHookMessageEx(Class _class, SEL sel, IMP imp, IMP *result)
+    __asm__("_MSHookMessageEx");
 
-void SubHookMessageEx(Class _class, SEL sel, IMP imp, IMP *result) {
+void MSHookMessageEx(Class _class, SEL sel, IMP imp, IMP *result) {
     int ret = substitute_hook_objc_message(_class, sel, imp, result, NULL);
     if (ret) {
-        substitute_panic("SubHookMessageEx: substitute_hook_objc_message returned %s\n",
+        substitute_panic("MSHookMessageEx: substitute_hook_objc_message returned %s\n",
                          substitute_strerror(ret));
     }
 }
